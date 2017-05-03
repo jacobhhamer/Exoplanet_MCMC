c2eefadc7ca823f6a1831e2d1341cf3781bc32be
Jacob Hamer <jacobhhamer@gmail.com>
2017-05-03 13:25:28 -0400

    Attempts to fix imports

--- exoplanet_mcmc/plot_test.py
+++ exoplanet_mcmc/plot_test.py
@@ -1,8 +1,8 @@
 import exoplanet_mcmc.exoplanet_mcmc as exmc
-import ..data.beta_pic_b as data
+from exoplanet_mcmc.data import beta_pic_b as dat
 import numpy as np
 
-obs=[[data.ts, data.del_RA, data.del_DEC]]
+obs=[[dat.ts, dat.del_RA, dat.del_DEC]]
 b=exmc.System(19.2, obs)
 f=b.plot_sky_positions()
 f.show()
